600380b9053b84fce60919d6165ce4608e791d2d
Andrew Donelick <adonelick@hmc.edu>
2015-04-11 15:27:47 -0700

    Added scaling and quicker operations to preprocessing code

--- regression/preprocessing.py
+++ regression/preprocessing.py
@@ -13,8 +13,10 @@ import numpy as np
 from util import *
 import time
 from pygeocoder import Geocoder
+import unicodedata
+from sklearn import preprocessing
 
-def preprocessData(trainData, testData, saveLocation=None):
+def preprocessData(trainData, testData, trainPath=None, testPath=None):
     """
     Preprocesses the train and test data files to 
     remove strings, time stamps, and other non-numerical
@@ -23,34 +25,14 @@ def preprocessData(trainData, testData, saveLocation=None):
 
     :param trainData: (Data object)
     :param testData: (2-d numpy array)
-    :param saveLocation: (string) Location to save new data files to
+    :param trainPath: (string) Location to save new train data file
+    :param testPath: (string) Location to save new test data file
     :return: (tuple of Data, 2-d numpy array) Preprocessed training/test data
     """
 
     # Generate a look-up table for all of the possible 
     # cities' lattitude and longitude in the data points.
-
-    cityLocations = {}
-
-    firstRow = True
-    for row in trainData.X:
-        if firstRow:
-            firstRow = False
-            continue
-
-        if not cityLocations.has_key(row[2]):
-            cityLocations[row[2]] = getLattitudeLongitude(row[2])
-            time.sleep(2)
-
-    for row in testData:
-        if firstRow:
-            firstRow = False
-            continue
-
-        if not cityLocations.has_key(row[2]):
-            cityLocations[row[2]] = getLattitudeLongitude(row[2])
-            time.sleep(2)
-
+    cityLocations = getCityLocations(trainData, testData)
 
     largerX = np.copy(trainData.X)
     largerX = np.insert(largerX, 3, 0, axis=1)
@@ -84,11 +66,9 @@ def preprocessData(trainData, testData, saveLocation=None):
 
     # Convert all other entries from strings to floats
     newTrainData.X = newTrainData.X[1:].astype(np.float)
+    newTrainData.X = preprocessing.scale(newTrainData.X)
     newTrainData.y = newTrainData.y[1:].astype(np.float)
 
-    print newTrainData.X.shape
-    print newTrainData.y.shape
-    
 
     # Now preprocess the testing (unlabeled) data
     newTestData = np.copy(testData)
@@ -120,8 +100,52 @@ def preprocessData(trainData, testData, saveLocation=None):
         newTestData[i][5] = dataType
 
     newTestData = newTestData[1:].astype(np.float)
+    newTestData = preprocessing.scale(newTestData)
+
+    # If desired, save the preprocesed data files
+    if trainPath != None:
+        X = newTrainData.X
+        y = newTrainData.y
+        completeData = np.insert(X, X.shape[1], y, axis=1)
+        np.savetxt(trainPath, completeData, delimiter=',')
+
+    if testPath != None:
+        np.savetxt(testPath, newTestData, delimiter=',')
 
     return newTrainData, newTestData
+
+def getCityLocations(trainData, testData):
+    """
+    Generate a look-up table for all of the possible 
+    cities' lattitude and longitude in the data points.
+
+    :param trainData: (Data object)
+    :param testData: (2-d numpy array)
+    :return: (dict) Mapping of city names to lattitude, longitude tuples
+    """
+
+    cityLocations = {}
+
+    firstRow = True
+    for row in trainData.X:
+        if firstRow:
+            firstRow = False
+            continue
+
+        if not cityLocations.has_key(row[2]):
+            cityLocations[row[2]] = getLattitudeLongitude(row[2])
+            time.sleep(2)
+
+    for row in testData:
+        if firstRow:
+            firstRow = False
+            continue
+
+        if not cityLocations.has_key(row[2]):
+            cityLocations[row[2]] = getLattitudeLongitude(row[2])
+            time.sleep(2)
+
+    return cityLocations
 
 def getLattitudeLongitude(city):
     """
@@ -131,10 +155,13 @@ def getLattitudeLongitude(city):
     :param city: (string) Name of city
     :return: (float tuple) lattitude and longitude of city
     """
+    city = unicode(city, encoding='utf_8')
+    city = unicodedata.normalize('NFKD', city).encode('ascii', 'ignore')
     try:
-        results = Geocoder.geocode(city)
+        results = Geocoder.geocode(city + ", Turkey")
         lattitude, longitude = results[0].coordinates
     except Exception, e:
+        print "Error for city: ", city
         lattitude, longitude = 0, 0
     return lattitude, longitude
 
--- regression/restaurant_revenue.py
+++ regression/restaurant_revenue.py
@@ -12,17 +12,40 @@ This file is meant as the main file predicting and evaluating restaurant revenue
 from util import *
 from preprocessing import *
 from semi_supervised_knn import *
- 
+
+# Paths for the data files
 DATA_PATH = "../data"
 TRAIN_PATH = DATA_PATH + "/train.csv"
 TEST_PATH = DATA_PATH + "/test.csv"
+PREPROCESSED_TRAIN_PATH = DATA_PATH + "/preprocessed_train.csv"
+PREPROCESSED_TEST_PATH = DATA_PATH + "/preprocessed_test.csv"
+
 
 def load_all_data():
+    """
+    Loads in the labeled (training) and unlabeled (testing)
+    data. The function automatically preprocesses the 
+    data according to the preprocessing file.
+
+    :return: (tuple of Data, 2-d numpy array) Preprocessed training/test data
+    """
+
+    try:
+        trainData = load_data(PREPROCESSED_TRAIN_PATH)
+        testData = load_data(PREPROCESSED_TEST_PATH, labeled=False).X
+
+        trainData.X = trainData.X.astype(np.float)
+        trainData.y = trainData.y.astype(np.float)
+        testData = testData.astype(np.float)
+
+    except Exception, e:   
+        labeled_data = load_data(TRAIN_PATH)
+        unlabeled_data = load_data(TEST_PATH, labeled=False).X
+        trainData, testData = preprocessData(labeled_data, unlabeled_data, 
+                                             PREPROCESSED_TRAIN_PATH, 
+                                             PREPROCESSED_TEST_PATH)
     
-    labeled_data = load_data(TRAIN_PATH)
-    unlabeled_data = load_data(TEST_PATH, labeled=False).X
-    
-    return preprocessData(labeled_data, unlabeled_data)
+    return trainData, testData
     
     
     
@@ -30,7 +53,7 @@ def main():
     
     labeled_data, unlabeled_data = load_all_data()
     
-    print labeled_data.X
+    print unlabeled_data
     
 
 if __name__ == '__main__':
